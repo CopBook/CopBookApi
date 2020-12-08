b3b896d1aac3abda21f8c53efd9ec725738132df
Josiah Sayers <josiah.sayers15@gmail.com>
2020-12-08 13:14:44 -0500

    allows updating profile from API

--- CopBookApi/Controllers/AuthController.cs
+++ CopBookApi/Controllers/AuthController.cs
@@ -2,7 +2,9 @@
 using CopBookApi.Interfaces.Services.Auth;
 using CopBookApi.Models.Api.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CopBookApi.Controllers
@@ -19,9 +21,36 @@ namespace CopBookApi.Controllers
         }
 
         [HttpPost]
-        public IAuthResponse RefreshToken()
+        public async Task<dynamic> RefreshToken(RefreshTokenRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await auth.RefreshToken(request);
+            }
+            catch (Exception e)
+            {
+                return HandleAuthException(e);
+            }
+        }
+
+        [HttpPost]
+        public async Task<dynamic> UpdateProfile(UpdateProfileRequest request)
+        {
+            try
+            {
+                StringValues idTokenFromHeader;
+                bool hasAuthHeader = HttpContext.Request.Headers.TryGetValue("Authorization", out idTokenFromHeader);
+                if (!hasAuthHeader) // TODO: Refactor this to a shared Authorization scheme once implemented
+                {
+                    throw new AuthFailedException("Id Token is required for this call", HttpStatusCode.Forbidden);
+                }
+                request.IdToken = idTokenFromHeader.ToString().Split(' ')[1];
+                return await auth.UpdateProfile(request);
+            }
+            catch (Exception e)
+            {
+                return HandleAuthException(e);
+            }
         }
 
         [HttpPost]
@@ -56,11 +85,19 @@ namespace CopBookApi.Controllers
             }
         }
 
-        private StatusCodeResult HandleAuthException(Exception e)
+        private IActionResult HandleAuthException(Exception e)
         {
             if (e is AuthFailedException exception)
             {
-                return StatusCode(exception.ResponseCode);
+                switch (exception.ResponseCode)
+                {
+                    case (int)HttpStatusCode.Forbidden:
+                        return new UnauthorizedObjectResult(new { exception.ErrorMessage });
+                    case (int)HttpStatusCode.BadRequest:
+                        return new BadRequestObjectResult(new { exception.ErrorMessage });
+                    default:
+                        return StatusCode(exception.ResponseCode);
+                }
             }
             else
             {
--- CopBookApi/Interfaces/Services/Auth/IAuthenticationService.cs
+++ CopBookApi/Interfaces/Services/Auth/IAuthenticationService.cs
@@ -7,5 +7,7 @@ namespace CopBookApi.Interfaces.Services.Auth
     {
         public Task<AuthResponse> SignUp(SignUpRequest request);
         public Task<AuthResponse> SignIn(SignInRequest request);
+        public Task<AuthResponse> UpdateProfile(UpdateProfileRequest request);
+        public Task<AuthResponse> RefreshToken(RefreshTokenRequest request);
     }
 }
