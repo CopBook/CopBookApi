0622db11cbf649cd08e7d0328cd027bc98add5bb
Nick Ater <nickolas.ater@gmail.com>
2020-12-13 23:51:54 -0500

    created cop.cs

---
+++ CopBookApi/Models/Api/Cop/Cop.cs
@@ -0,0 +1,18 @@
+﻿using CopBookApi.Interfaces.Api.Cop;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopBookApi.Models.Api
+{
+    public class Cop : ICop
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BadgeId { get; set; }
+    }
+}
