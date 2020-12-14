5ca15c9d573a1ae15f0c7e1ca68c320c2a4af8e2
Nick Ater <nickolas.ater@gmail.com>
2020-12-13 23:51:31 -0500

    created cop controller with basic CR... but no UD.. yet

---
+++ CopBookApi/Controllers/CopController.cs
@@ -0,0 +1,43 @@
+﻿using CopBookApi.Interfaces.Api.Cop;
+using CopBookApi.Interfaces.Controllers;
+using CopBookApi.Models.Api;
+using CopBookApi.Services;
+using CopBookApi.Services.DTO;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopBookApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CopController : Controller
+    {
+        readonly CopDTO copDTO;
+        public CopController(IMongoClient client)
+        {
+            copDTO = new CopDTO(client);
+        }
+
+        public async Task<List<Cop>> GetAllCops()
+        {
+            return await copDTO.GetAllCops();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task<Cop> GetCop(Guid id)
+        {
+            return copDTO.GetCopById(id);
+        }
+
+        public async Task CreateCop(Cop cop)
+        {
+            await copDTO.CreateCop(cop);
+        }
+
+    }
+}
