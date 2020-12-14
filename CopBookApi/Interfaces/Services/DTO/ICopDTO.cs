61db9b15ed8bd9574dc03610321a77f996f2fe44
Nick Ater <nickolas.ater@gmail.com>
2020-12-13 23:49:43 -0500

    created copDTO interface

---
+++ CopBookApi/Interfaces/Services/DTO/ICopDTO.cs
@@ -0,0 +1,26 @@
+﻿using CopBookApi.Interfaces.Api.Cop;
+using CopBookApi.Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopBookApi.Interfaces.Services.DTO
+{
+    public interface ICopDTO
+    {
+        // CREATE
+        Task CreateCop(Cop cop);
+
+        // READ
+        Task<Cop> GetCopById(Guid id);
+
+        Task<List<Cop>> GetAllCops();
+
+        // UDPATE
+        Task UpdateCop<T>(Guid id, string fieldToUpdate, T updatedValue);
+
+        // DELETE
+        Task DeleteCop(Guid id);
+    }
+}
