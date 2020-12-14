7e3f2a5c37f226c303c192ae05ef3b5452a38e66
Nick Ater <nickolas.ater@gmail.com>
2020-12-13 23:50:37 -0500

    created ICop

---
+++ CopBookApi/Interfaces/Api/Cop/ICop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CopBookApi.Interfaces.Api.Cop
+{
+    public interface ICop
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BadgeId { get; set; }
+
+    }
+}
